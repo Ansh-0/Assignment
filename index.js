96d9d8a56e1477adf92a1226701c605213713b11
Ansh <anshg1921@gmail.com>
2023-07-08 18:32:13 +0530

    Used a Modal

---
+++ index.js
@@ -0,0 +1,34 @@
+function openForm(imageID) {
+	var imageSrc = document.getElementById(imageID).src;
+	var modalImage = document.getElementById('modalImage');
+	var formImage = document.getElementById('formImage');
+	var formModal = document.getElementById('formModal');
+
+	modalImage.src = imageSrc;
+	formImage.value = imageSrc;
+	formModal.style.display = 'block';
+}
+
+function closeForm() {
+	var formModal = document.getElementById('formModal');
+	formModal.style.display = 'none';
+}
+
+document
+	.getElementById('uploadForm')
+	.addEventListener('submit', function (event) {
+		event.preventDefault();
+
+		var form = event.target;
+		var formData = new FormData(form);
+
+		// Perform AJAX request using Axios
+		axios
+			.post('./api/form', formData)
+			.then(function (response) {
+				closeForm();
+			})
+			.catch(function (error) {
+				console.log('error');
+			});
+	});
